d3f8ea2e6cab006d266c9a818c5a4f8688154d8a
cgodumnnabuike <ebubeodum@gmail.com>
2023-04-05 18:23:34 +0100

    Deleted customers table

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Visitor;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory()
+        ->count(50)
+        ->create();
+    }
+}
