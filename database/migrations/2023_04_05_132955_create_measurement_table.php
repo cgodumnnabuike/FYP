11b5aca2361a4db6d3fe5576902d830cbb395857
cgodumnnabuike <ebubeodum@gmail.com>
2023-04-05 14:34:00 +0100

    All tables migration created

--- database/migrations/2023_04_05_132955_create_measurement_table.php
+++ database/migrations/2023_04_05_132955_create_measurement_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('measurement', function (Blueprint $table) {
             $table->id();
-            $table->string('Firstname');
-            $table->string('LastName');
-            $table->string('Email')->unique();
-            $table->timestamp('Email_verifield_at')->nullable();
-            $table->string('Password');
+            $table->unsignedBigInteger('meter_id');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('measurement');
     }
 };
