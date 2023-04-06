dc0755c4b83bd5ab1813645652e16b886bda9f2f
cgodumnnabuike <ebubeodum@gmail.com>
2023-04-06 13:29:05 +0100

    guest layout page modified

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,10 +12,48 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/style.scss'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        {{-- Navigation Bar --}}
+        <nav class="navbar navbar-expand-sm fixed-top bg-gray-150">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/welcome.html">EMA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                    aria-controls="offcanvasNavbar">
+                    <span id="menu-icon" class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">X
+                        </button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/index.html">Why to choose EMA</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/register">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/login">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#section-c">About Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 dark:bg-gray-900">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -26,5 +64,15 @@
                 {{ $slot }}
             </div>
         </div>
+        {{-- footer --}}
+
+        <footer class="text-center position-relative footer py-3">
+            <div class="container-fluid">
+                <div>
+                    <p>Designed for you to save</p>
+                    <p class="lead"> Copyright &copy; EMA management webapp
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
