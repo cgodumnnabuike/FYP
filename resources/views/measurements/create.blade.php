7d164a0bf249a480ff30cc292282764fc5a537db
cgodumnnabuike <ebubeodum@gmail.com>
2023-04-19 14:16:21 +0100

    form defined

--- resources/views/measurements/create.blade.php
+++ resources/views/measurements/create.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
-    <form action="{{ route('measurements.store') }}" method="POST">
-        @csrf
-
-        <div class="form-group">
-            <label for="meter_number">Meter Number</label>
-            <input type="text" name="meter_number" class="form-control" required>
+    <section class="p-5">
+        <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <h1>Add measurement</h1>
+            </div>
         </div>
-
-        <!-- Add other fields for measurement data here -->
-
-        <button type="submit" class="btn btn-primary">Add Measurement Data</button>
-    </form>
+        <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-app-layout>
